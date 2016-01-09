97f9d93f4783d8c6fbe1ac96214a7d4a4ab089a1
Dustin <dustinsinkey1@gmail.com>
2016-01-09 15:48:49 -0800

    Create CollectionStore and added prive functions that mutate collectionTweets

---
+++ source/stores/CollectionStore.js
@@ -0,0 +1,28 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+
+var CHANGE_EVENT = 'change';
+
+var collectionTweets = {};
+
+var collectionName = 'new';
+
+function addTweetToCollection(tweet){
+	collectionTweets[tweet.id] = tweet;
+}
+
+function removeTweetFromCollection(tweetId){
+	delete collectionTweets[tweetId];
+}
+
+function removeAllTweetsFromCollection(){
+	collectionTweets = {};
+}
+
+function setCollectionName(name){
+	collectionName = name;
+}
+
+
+module.exports = CollectionStore;
