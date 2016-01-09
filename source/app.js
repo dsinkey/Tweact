7a502feaa42e1623bd3d61dea4441162ff45580a
Dustin <dustinsinkey1@gmail.com>
2016-01-09 14:48:26 -0800

    Update app.js to use WebAPIUtils.js

--- source/app.js
+++ source/app.js
@@ -1,6 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Application = require('./components/Application.react');
+var WebApiUtils = require('./utils/WebApiUtils');
+
+WebApiUtils.initializeStreamOfTweets();
 
 ReactDOM.render(<Application />, document.getElementById('react-application'));
 
